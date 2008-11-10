953cf2dd78c05fa085e5a075a7dc0262c6c1b821
logikal16 <logikal16@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-10 01:02:27 +0000

    Initial import
    git-svn-id: https://plugins.svn.wordpress.org/upgrade-ie/trunk@73126 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ init.php
@@ -0,0 +1,72 @@
+<?php
+/*
+Plugin Name: Upgrade IE
+Plugin URI: http://uproot.us/
+Description: Get your users to ditch IE!
+Version: 1.0.0
+Author: Matt Gibbs
+Author URI: http://uproot.us/
+
+Copyright 2008  Matt Gibbs  (email : logikal16@gmail.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+function show_banner()
+{
+    $cookie = $_COOKIE['hideAlert'];
+    $browser = $_SERVER['HTTP_USER_AGENT'];
+    if (empty($cookie) &&
+        (false !== strpos($browser, 'MSIE 5') || false !== strpos($browser, 'MSIE 6')))
+    {
+?>
+<script type="text/javascript">
+function createCookie() {
+    var hours = 12;
+    var date = new Date();
+    date.setTime(date.getTime()+(hours*3600000));
+    document.cookie = "hideAlert=1; expires="+date.toGMTString()+"; path=/";
+    jQuery("#uIE").fadeOut();
+}
+</script>
+<style type="text/css">
+#uIE #hover {
+    top: 0;
+    width: 100%;
+    height: 24px;
+    position: absolute;
+    text-align: center;
+    background: #FFFFC8;
+}
+
+#uIE img {
+    vertical-align: middle;
+}
+</style>
+<div id="uIE">
+    <div id="hover">
+        <div style="float:right; padding-right:10px"><a href="#" onclick="createCookie()">hide</a></div>
+        <img src="/wp-content/plugins/upgrade-IE/booey.png" alt="booey" />
+        This site looks a lot better on an upgraded browser. <a href="http://browsehappy.com/" target="blank">How do I upgrade?</a>
+    </div>
+    <div style="height:10px"><!--clear--></div>
+</div>
+<?php
+    }
+}
+
+// Hook for displaying the banner
+add_action('wp_head', 'show_banner');
+
---
+++ readme.txt
@@ -0,0 +1,18 @@
+=== Pods ===
+Contributors: logikal16
+Tags: upgrade, ie6
+Requires at least: 2.6
+Tested up to: 2.7
+Stable tag: trunk
+
+Notify site visitors to upgrade their browser.
+
+== Description ==
+
+This plugin nofifies users that they should upgrade their browser. This plugin adds a small pane to the top of the page alerting the user to upgrade their browser. The pane can optionally be hidden by the user.
+
+== Installation ==
+
+= Installation =
+1. Unzip the `upgrade-ie` folder into `wp-content/plugins/`
+2. Enable the plugin through Plugins > Installed
