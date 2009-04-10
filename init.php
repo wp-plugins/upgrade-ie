30973aae470d69b743845d312e2bb1f5e0d9510f
logikal16 <logikal16@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-10 12:26:19 +0000

    Added admin page to edit label
    git-svn-id: https://plugins.svn.wordpress.org/upgrade-ie/trunk@109353 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- init.php
+++ init.php
@@ -3,11 +3,11 @@
 Plugin Name: Upgrade IE
 Plugin URI: http://wordpress.org/extend/plugins/upgrade-ie/
 Description: Notify IE users to upgrade their browser.
-Version: 1.0.2
+Version: 1.0.3
 Author: Matt Gibbs
 Author URI: http://wordpress.org/extend/plugins/upgrade-ie/
 
-Copyright 2008  Matt Gibbs  (email : logikal16@gmail.com)
+Copyright 2009  Matt Gibbs  (email : logikal16@gmail.com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -28,6 +28,11 @@ function show_banner()
 {
     $cookie = $_COOKIE['hideAlert'];
     $browser = $_SERVER['HTTP_USER_AGENT'];
+    $uie_label = get_option('uie_label');
+    if (empty($uie_label))
+    {
+        $uie_label = 'This site looks a lot better on an upgraded browser. <a href="http://browsehappy.com/" target="blank">How do I upgrade?</a>';
+    }
     if (empty($cookie) &&
         (false !== strpos($browser, 'MSIE 5') || false !== strpos($browser, 'MSIE 6')))
     {
@@ -65,8 +70,7 @@ function createCookie() {
     <div id="hover">
         <img src="/wp-content/plugins/upgrade-ie/close.gif" alt="close" id="hide" onclick="createCookie()" />
         <img src="/wp-content/plugins/upgrade-ie/alert.gif" alt="alert" />
-        This site looks a lot better on an upgraded browser.
-        <a href="http://browsehappy.com/" target="blank">How do I upgrade?</a>
+        <?php echo $uie_label; ?>
     </div>
     <div style="height:10px"><!--clear--></div>
 </div>
@@ -74,6 +78,35 @@ function createCookie() {
     }
 }
 
-// Hook for displaying the banner
-add_action('wp_head', 'show_banner');
+function uIE_menu_page()
+{
+?>
+<div class="wrap">
+    <h2>Upgrade IE</h2>
+    <form method="post" action="options.php">
+        <?php wp_nonce_field('update-options'); ?>
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row">Alert Label</th>
+                <td>
+                    <input type="text" name="uie_label" value="<?php echo get_option('uie_label'); ?>" />
+                </td>
+            </tr>
+        </table>
+        <input type="hidden" name="action" value="update" />
+        <input type="hidden" name="page_options" value="uie_label" />
+        <p class="submit">
+            <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+        </p>
+    </form>
+</div>
+<?php
+}
 
+add_action('wp_head', 'show_banner');
+add_action('admin_menu', 'uIE_menu');
+
+function uIE_menu()
+{
+    add_management_page('Upgrade IE', 'Upgrade IE', 8, 'upgrade-ie', 'uIE_menu_page');
+}
--- readme.txt
+++ readme.txt
@@ -1,8 +1,8 @@
 === Upgrade IE ===
 Contributors: logikal16
-Tags: upgrade, ie6
+Tags: upgrade, ie6, ie, browser, compatibility
 Requires at least: 2.6
-Tested up to: 2.7
+Tested up to: 2.7.x
 Stable tag: trunk
 
 Notify IE users to upgrade their browser.
@@ -11,10 +11,10 @@ Notify IE users to upgrade their browser.
 
 [See the plugin in action](http://pods.uproot.us) (only appears in IE6 and below)
 
-This plugin notifies users that they should upgrade their browser. It adds a small pane to the top of the page alerting users to upgrade their browser. The pane can optionally be hidden by the user.
+This plugin notifies users that they should upgrade their browser. It adds a small pane to the top of the page alerting users to upgrade their browser. The pane can optionally be hidden by the user. The alert message can also optionally be changed.
 
 == Installation ==
 
 = Installation =
-1. Unzip the upgrade-ie folder into `wp-content/plugins/`
+1. Unzip the upgrade-ie folder into wp-content/plugins/
 2. Activate the plugin through Plugins > Installed
